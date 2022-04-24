7fa27beb8609cf2672fde108ea0c29ac88bdfbb1
ealpizarp <ericalpizar@gmail.com>
2022-04-23 19:45:25 -0600

    Support for hosted backend

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://master.d3eu48e9jqn9ws.amplifyapp.com",
     credentials: true,
   },
 });
